5aed5b2eab8cbcf17d19bbea2c77fa17b08c528f
aries0420 <aries0420@server.fake>
2013-07-01 17:08:22 -0700

    Adding bonus code challenge
    Output can be found on heroku:
    http://agile-hamlet-8041.herokuapp.com/users

---
+++ manageuser/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manageuser::Application.config.secret_token = 'a8b03a475f126ef953c9e7e046e73089a6f892c0eeb6fa9de10c288494957d9343fb9a4b47331f7a1e1ed10caa635ce604c61076d33952c66c54e5ed02de374d'
